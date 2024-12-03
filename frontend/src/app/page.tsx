bb6e703f1df22fd8202dd6b3acd5b0bb1d5c6bec
Oleksandr <zagrebelniysasha@gmail.com>
2024-12-03 17:38:43 +0200

    feat: add background and heading

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,18 +4,22 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="min-h-screen bg-black">
-      {/* Main Content */}
-      <div className="flex flex-col items-center justify-center min-h-screen bg-pink-200 relative">
-        {/* Navigation Buttons */}
-        <div className="absolute top-8 right-8 flex space-x-4 items-center">
-          <Link href="/login" className="text-black font-semibold">Log in</Link>
+      <div
+        className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center relative"
+        style={{ backgroundImage: "url('/backgrounds/image 2.jpg')" }}
+      >
+        <div className="absolute inset-0 bg-black bg-opacity-20"></div>
+
+        <div className="absolute top-8 right-8 flex space-x-4 items-center z-10 gap-4">
+          <Link href="/login" className="text-white font-semibold">
+            Log in
+          </Link>
           <Link href="/register" className="px-4 py-1 bg-white rounded-full font-semibold shadow">
             Sign up
           </Link>
         </div>
 
-        {/* Background Placeholder */}
-        <h1 className="text-3xl font-bold text-black">background image</h1>
+        <h1 className="text-9xl font-bold text-white text-shadow z-10">ExcursionMate</h1>
       </div>
     </div>
   );
