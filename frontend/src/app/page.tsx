1f6b12eeb9f09d228e1e95d22a32b2cbc20d90f8
Oleksandr <zagrebelniysasha@gmail.com>
2024-12-03 17:45:07 +0200

    feat: add linlk animations

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,19 +3,19 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black">
+    <div className="min-h-screen bg-black font-poppins">
       <div
         className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center relative"
         style={{ backgroundImage: "url('/backgrounds/image 2.jpg')" }}
       >
         <div className="absolute inset-0 bg-black bg-opacity-20"></div>
 
-        <div className="absolute top-8 right-8 flex space-x-4 items-center z-10 gap-4">
-          <Link href="/login" className="text-white font-semibold">
-            Log in
+        <div className="absolute top-8 right-8 text-lg flex space-x-4 items-center z-10 gap-4">
+          <Link href="/login" className="px-4 py-1 text-white rounded-full font-semibold hover:bg-white hover:text-black transition duration-300 ease-in-out">
+            Увійти
           </Link>
-          <Link href="/register" className="px-4 py-1 bg-white rounded-full font-semibold shadow">
-            Sign up
+          <Link href="/register" className="px-4 py-1 bg-white rounded-full font-semibold shadow hover:bg-black hover:text-white transition duration-300 ease-in-out">
+            Зареєструватись
           </Link>
         </div>
 
