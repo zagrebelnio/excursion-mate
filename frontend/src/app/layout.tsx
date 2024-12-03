74beba6b3e031a724dc587e3ecaf483bafabc11a
Oleksandr <zagrebelniysasha@gmail.com>
2024-12-03 16:26:31 +0200

    feat: set up font poppins

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+})
 
 export const metadata: Metadata = {
   title: "Excursion Mate",
@@ -14,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className="antialiased"
+        className={`${poppins.className} antialiased`}
       >
         {children}
       </body>
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
     },
   },
   plugins: [],
