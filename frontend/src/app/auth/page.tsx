794ae4b8861138bec81cfc29c940b8f1fcc170a2
Oleksandr <zagrebelniysasha@gmail.com>
2024-12-04 03:57:41 +0200

    feat: add login form

--- frontend/src/app/auth/page.tsx
+++ frontend/src/app/auth/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useSearchParams } from 'next/navigation';
 import RegistrationForm from '../components/registrationForm';
+import LoginForm from '../components/loginForm';
 
 export default function Register() {
   const searchParams = useSearchParams();
@@ -9,7 +10,8 @@ export default function Register() {
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-5">
       <h1 className="text-6xl font-bold text-black">ExcursionMate</h1>
-      {mode === 'register' && <RegistrationForm />}  
+      {mode === 'register' && <RegistrationForm />}
+      {mode === 'login' && <LoginForm />} 
     </div>
   );
 }
