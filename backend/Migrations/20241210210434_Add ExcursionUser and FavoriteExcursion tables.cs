57b8bd059cbb686fffee002ee7f11a7ee049cbdf
Hrytsenko Artem <hrytsenko.artem@lll.kpi.ua>
2024-12-10 23:13:45 +0200

    db: added 2 tables

---
+++ backend/Migrations/20241210210434_Add ExcursionUser and FavoriteExcursion tables.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExcursionUserandFavoriteExcursiontables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExcursionUsers",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ExcursionId = table.Column<int>(type: "int", nullable: false),
+                    RegistrationDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExcursionUsers", x => new { x.UserId, x.ExcursionId });
+                    table.ForeignKey(
+                        name: "FK_ExcursionUsers_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ExcursionUsers_Excursions_ExcursionId",
+                        column: x => x.ExcursionId,
+                        principalTable: "Excursions",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FavoriteExcursions",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ExcursionId = table.Column<int>(type: "int", nullable: false),
+                    AddedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FavoriteExcursions", x => new { x.UserId, x.ExcursionId });
+                    table.ForeignKey(
+                        name: "FK_FavoriteExcursions_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_FavoriteExcursions_Excursions_ExcursionId",
+                        column: x => x.ExcursionId,
+                        principalTable: "Excursions",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExcursionUsers_ExcursionId",
+                table: "ExcursionUsers",
+                column: "ExcursionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FavoriteExcursions_ExcursionId",
+                table: "FavoriteExcursions",
+                column: "ExcursionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExcursionUsers");
+
+            migrationBuilder.DropTable(
+                name: "FavoriteExcursions");
+        }
+    }
+}
