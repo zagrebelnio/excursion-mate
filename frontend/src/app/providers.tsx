8cd6b6a7366223b08b42a5335eba9de9e2b7d64a
Oleksandr <zagrebelniysasha@gmail.com>
2024-12-13 00:32:40 +0200

    delete: filters context

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,14 +1,11 @@
 'use client';
 import { SessionProvider } from 'next-auth/react';
-import { FiltersProvider } from '@/context/filtersContext';
 import { UserProvider } from '@/context/userContext';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <FiltersProvider>
-      <SessionProvider>
-        <UserProvider>{children}</UserProvider>
-      </SessionProvider>
-    </FiltersProvider>
+    <SessionProvider>
+      <UserProvider>{children}</UserProvider>
+    </SessionProvider>
   );
 }
